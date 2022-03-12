c8f344c0a538c642ec16b0673e91e8ad1fa6bcac
Bugo <bugo@dragomano.ru>
2022-03-13 00:35:17 +0500

    Add Serbian translation

---
+++ Themes/default/languages/MessageBookmarks.serbian_latin.php
@@ -0,0 +1,26 @@
+<?php
+
+$txt['mb_title'] = 'Obeležavanje poruke';
+$txt['mb_subject'] = 'Ime obeležene poruke';
+$txt['mb_note'] = 'Napomena (opciono)';
+$txt['mb_popup_bookmarks'] = 'Moje obeležene poruke';
+$txt['mb_show_bookmarks'] = 'Prikaži obeleživače poruka';
+$txt['mb_settings'] = 'Obeleživači poruka';
+$txt['mb_mod_desc'] = 'Ovde možete promeniti podešavanja moda obeleživača poruka.';
+
+$txt['mb_ignore_boards'] = 'Zanemareni forumi';
+$txt['mb_ignored_boards_desc'] = 'Izaberite forume na kojima obeleživač poruka neće raditi';
+$txt['mb_class'] = 'Klasa za isticanje obeleženih poruka u temama';
+
+$txt['permissiongroup_message_bookmarks'] = $txt['permissiongroup_simple_message_bookmarks'] = 'Obeleživači poruka';
+$txt['permissionname_use_message_bookmarks'] = 'Korišćenje obeleživača';
+$txt['permissionhelp_use_message_bookmarks'] = 'Sa ovom dozvolom korisnici će moći da pregledaju, dodaju i uklanjaju obeleživače poruka.';
+$txt['groups_use_message_bookmarks'] = 'Dozvole za korišćenje obeleživača poruka';
+$txt['cannot_use_message_bookmarks'] = 'Nažalost, nemate pravo da obeležavate poruke.';
+
+$txt['mb_profile_title'] = $txt['permissiongroup_message_bookmarks'];
+$txt['mb_profile_desc'] = 'Ovde možete da vidite svoje obeležene poruke.';
+$txt['mb_no_items'] = 'Još uvek nema sačuvanih poruka.';
+
+$txt['mb_add_bookmark'] = '💚 Sačuvaj poruku';
+$txt['mb_remove_bookmark'] = '💔 Ukloni poruku iz sačuvanih';
