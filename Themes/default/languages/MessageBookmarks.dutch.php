2e1a87d19881782e945686485d21211c6e174c4c
Bugo <bugo@dragomano.ru>
2022-03-30 17:27:35 +0500

    Add Dutch translation

---
+++ Themes/default/languages/MessageBookmarks.dutch.php
@@ -0,0 +1,32 @@
+<?php
+
+$txt['mb_title'] = 'Bladwijzer maken';
+$txt['mb_subject'] = 'Naam bladwijzer';
+$txt['mb_note'] = 'Opmerking (optioneel)';
+$txt['mb_popup_bookmarks'] = 'Mijn bladwijzers';
+$txt['mb_show_bookmarks'] = 'Berichtenbladwijzers weergeven';
+$txt['mb_settings'] = 'Berichtenbladwijzers';
+$txt['mb_mod_desc'] = 'Hier kunt u de instellingen van de Message Bookmarks mod wijzigen.';
+
+$txt['mb_ignore_boards'] = 'Genegeerde boards';
+$txt['mb_ignored_boards_desc'] = 'Selecteer de boards waarin Message Bookmarks NIET zouden moeten werken';
+$txt['mb_class'] = 'Een class voor het markeren van berichten met bladwijzers in topics';
+$txt['mb_add_icon'] = 'Icoon van het toevoegen van een bladwijzer';
+$txt['mb_del_icon'] = 'Icoon voor het verwijderen van een bladwijzer';
+$txt['mb_icon_subtext'] = 'Je kunt emoji of FA code gebruiken zoals <i>fas fa-bookmark</i>';
+
+$txt['permissiongroup_message_bookmarks'] = $txt['permissiongroup_simple_message_bookmarks'] = 'Berichtenbladwijzers';
+$txt['permissionname_use_message_bookmarks'] = 'Bladwijzers gebruiken';
+$txt['permissionhelp_use_message_bookmarks'] = 'Met deze permissie kunnen gebruikers bladwijzers voor berichten bekijken, toevoegen en verwijderen.';
+$txt['groups_use_message_bookmarks'] = 'Permissies voor het gebruik van bladwijzers voor berichten';
+$txt['cannot_use_message_bookmarks'] = 'Helaas heeft u niet het recht om bladwijzers voor berichten te maken.';
+
+$txt['mb_profile_title'] = $txt['permissiongroup_message_bookmarks'];
+$txt['mb_profile_desc'] = 'Je kunt je bladwijzers hier zien.';
+$txt['mb_no_items'] = 'Er zijn nog geen bladwijzers.';
+
+$txt['mb_add_bookmark'] = 'Voeg bladwijzer toe';
+$txt['mb_remove_bookmark'] = 'Bladwijzer verwijderen';
+
+$txt['top_replies'] = 'Topics met de meeste bladwijzers';
+$txt['top_members'] = 'Gebruikers met de meeste bladwijzers';
