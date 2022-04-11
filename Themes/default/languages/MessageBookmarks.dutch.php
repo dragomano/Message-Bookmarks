dd52406f5bd17c7916d80a6512eecdd1dde7cf5f
Bugo <bugo@dragomano.ru>
2022-04-11 05:55:47 +0500

    Update Dutch

--- Themes/default/languages/MessageBookmarks.dutch.php
+++ Themes/default/languages/MessageBookmarks.dutch.php
@@ -1,6 +1,6 @@
 <?php
 
-$txt['mb_title'] = 'Bladwijzer maken';
+$txt['mb_title'] = 'Bladwijzer informatie';
 $txt['mb_subject'] = 'Naam bladwijzer';
 $txt['mb_note'] = 'Opmerking (optioneel)';
 $txt['mb_popup_bookmarks'] = 'Mijn bladwijzers';
@@ -11,22 +11,26 @@ $txt['mb_mod_desc'] = 'Hier kunt u de instellingen van de Message Bookmarks mod 
 $txt['mb_ignore_boards'] = 'Genegeerde boards';
 $txt['mb_ignored_boards_desc'] = 'Selecteer de boards waarin Message Bookmarks NIET zouden moeten werken';
 $txt['mb_class'] = 'Een class voor het markeren van berichten met bladwijzers in topics';
-$txt['mb_add_icon'] = 'Icoon van het toevoegen van een bladwijzer';
+$txt['mb_add_icon'] = 'Icoon voor het toevoegen van een bladwijzer';
 $txt['mb_del_icon'] = 'Icoon voor het verwijderen van een bladwijzer';
 $txt['mb_icon_subtext'] = 'Je kunt emoji of FA code gebruiken zoals <i>fas fa-bookmark</i>';
+$txt['mb_show_top_messages_stats'] = 'Berichten met veel bladwijzers weergeven';
+$txt['mb_show_top_messages_count'] = 'Het minimum aantal berichten dat bovenaan moet worden weergegeven';
+$txt['mb_show_top_members_stats'] = 'Gebruikers weergeven met de meeste bladwijzers';
+$txt['mb_show_top_members_count'] = 'Het minimum aantal bladwijzers dat gebruikers bovenaan moeten weergeven';
 
-$txt['permissiongroup_message_bookmarks'] = $txt['permissiongroup_simple_message_bookmarks'] = 'Berichtenbladwijzers';
+$txt['permissiongroup_message_bookmarks'] = $txt['permissiongroup_simple_message_bookmarks'] = 'Bericht bladwijzers';
 $txt['permissionname_use_message_bookmarks'] = 'Bladwijzers gebruiken';
 $txt['permissionhelp_use_message_bookmarks'] = 'Met deze permissie kunnen gebruikers bladwijzers voor berichten bekijken, toevoegen en verwijderen.';
 $txt['groups_use_message_bookmarks'] = 'Permissies voor het gebruik van bladwijzers voor berichten';
-$txt['cannot_use_message_bookmarks'] = 'Helaas heeft u niet het recht om bladwijzers voor berichten te maken.';
+$txt['cannot_use_message_bookmarks'] = 'Je hebt helaas geen rechten om bladwijzers voor berichten te maken.';
 
 $txt['mb_profile_title'] = $txt['permissiongroup_message_bookmarks'];
-$txt['mb_profile_desc'] = 'Je kunt je bladwijzers hier zien.';
+$txt['mb_profile_desc'] = 'Op deze pagina zie je een overzicht van alle bladwijzers die je hebt gemaakt.';
 $txt['mb_no_items'] = 'Er zijn nog geen bladwijzers.';
 
-$txt['mb_add_bookmark'] = 'Voeg bladwijzer toe';
+$txt['mb_add_bookmark'] = 'Bladwijzer maken';
 $txt['mb_remove_bookmark'] = 'Bladwijzer verwijderen';
 
-$txt['top_replies'] = 'Topics met de meeste bladwijzers';
+$txt['top_replies'] = 'Berichten met de meeste bladwijzers';
 $txt['top_members'] = 'Gebruikers met de meeste bladwijzers';
